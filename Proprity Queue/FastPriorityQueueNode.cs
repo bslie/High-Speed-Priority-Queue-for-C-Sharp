501a89fafeaa0ba7d8f2e0704c07eba7cb4c5343
Krovlin Sergey <krovlin97@yandex.ru>
2024-01-12 05:49:35 +0300

    Init fork

--- Proprity Queue/FastPriorityQueueNode.cs
+++ Proprity Queue/FastPriorityQueueNode.cs
@@ -1,23 +1,19 @@
-﻿namespace Priority_Queue
+﻿using System;
+
+namespace Priority_Queue
 {
-    public class GenericPriorityQueueNode<TPriority>
+    public class FastPriorityQueueNode
     {
         /// <summary>
         /// The Priority to insert this node at.
         /// Cannot be manually edited - see queue.Enqueue() and queue.UpdatePriority() instead
         /// </summary>
-        public TPriority Priority { get; protected internal set; }
+        public float Priority { get; protected internal set; }
 
         /// <summary>
         /// Represents the current position in the queue
         /// </summary>
         public int QueueIndex { get; internal set; }
-
-        /// <summary>
-        /// Represents the order the node was inserted in
-        /// </summary>
-        public long InsertionIndex { get; internal set; }
-
 
 #if DEBUG
         /// <summary>
